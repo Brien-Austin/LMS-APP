92eebdf6a01ac7c9a96f002aebe43489e67e7dcb
Brien-Austin <brienaustinclayton@gmail.com>
2024-12-20 15:28:05 +0530

    hooks js conversion

---
+++ client/src/hooks/useRoutes.js
@@ -0,0 +1,31 @@
+import { useMemo } from "react";
+import { Home, TvMinimalPlay, UserRound } from "lucide-react";
+
+export const useRoutes = () => {
+  const routes = useMemo(
+    () => [
+      {
+        label: "Home",
+        icon: Home,
+        route: "/",
+      },
+      {
+        label: "My Courses",
+        icon: TvMinimalPlay,
+        route: "/mycourses",
+      },
+      {
+        label: "Profile",
+        icon: UserRound,
+        route: "/profile",
+      },
+      // Uncomment if needed
+      // {
+      //     label : 'Instructors',
+      //     icon : Teacher
+      // }
+    ],
+    []
+  );
+  return routes;
+};
